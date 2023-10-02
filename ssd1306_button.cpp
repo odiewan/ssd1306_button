e0f03c9d07e342c1100bca47e4abf7c8c9d3cbbd
odiewan <odie@odiesystems.com>
2023-10-02 10:51:54 -0700

    Implement selectable font size

--- ssd1306_button.cpp
+++ ssd1306_button.cpp
@@ -4,20 +4,41 @@
 #include <Adafruit_SSD1306.h>
 #include "ssd1306_button.h"
 
+
+
+uint16_t textWidths[] {
+    6,
+    12,
+    18,
+    24
+};
+
+uint16_t textHeights[] {
+    8,
+    16,
+    24,
+    32
+};
+
+
+
 //=================================================================================================
 void ssd1306_btn::constructorCore() {
     sbtnDisplay = NULL;
 
-
     sbtnState = false;
     sbtnEnable = true;
-    sbtnWidth = 24;
-    sbtnHeight = 8;
+    sbtnTxtSize = 1;
     sbtnX = 0;
     sbtnY = 0;
-    sbtnTxtX = sbtnX + xPAD;
+    sbtnTxtX = sbtnX + XPAD;
     sbtnTxtY = sbtnY + YPAD;
     sbtnText = "btnText";
+    calcTextDims();
+    sbtnBGColor = SSD1306_WHITE;
+    sbtnTextColor = SSD1306_BLACK;
+
+    // sbtnDisplay->setTextColor(sbtnTextColor);
 }
 
 //=================================================================================================
@@ -27,20 +48,34 @@ ssd1306_btn::ssd1306_btn()
 }
 
 //=================================================================================================
-ssd1306_btn::ssd1306_btn(Adafruit_SSD1306 *nDsp, int nX, int nY, int nH, int nW, String nText, bool nState)
+ssd1306_btn::ssd1306_btn(Adafruit_SSD1306 *nDsp, int16_t nX, int16_t nY, int16_t nH, int16_t nW, String nText, bool nState)
 {
     constructorCore();
     sbtnDisplay = nDsp;
     sbtnState = nState;
+    sbtnText = nText;
     sbtnWidth = nW;
     sbtnHeight = nH;
     sbtnX = nX;
     sbtnY = nY;
-    sbtnTxtX = sbtnX + xPAD;
+    sbtnTxtX = sbtnX + XPAD;
     sbtnTxtY = sbtnY + YPAD;
-    sbtnText = nText;
+    calcTextDims();
 }
 
+//=================================================================================================
+ssd1306_btn::ssd1306_btn(Adafruit_SSD1306 *nDsp, int16_t nX, int16_t nY, uint8_t nTxtSz, String nText, bool nState) {
+    constructorCore();
+    sbtnDisplay = nDsp;
+    sbtnState = nState;
+    sbtnText = nText;
+    sbtnX = nX;
+    sbtnY = nY;
+    sbtnTxtX = sbtnX + XPAD;
+    sbtnTxtY = sbtnY + YPAD;
+    sbtnTxtSize = nTxtSz;
+    calcTextDims();
+}
 //=================================================================================================
 void ssd1306_btn::enable() {
     sbtnEnable = true;
@@ -63,9 +98,17 @@ void ssd1306_btn::render(bool nState) {
 
 //=================================================================================================
 void ssd1306_btn::renderText() {
-    sbtnDisplay->print("*" + sbtnText + "*");
+    sbtnDisplay->print(sbtnText);
 }
 
+//=================================================================================================
+void ssd1306_btn::calcTextDims() {
+    sbtnTxtLen = sbtnText.length();
+    sbtnTextWd = sbtnTxtLen * textWidths[sbtnTxtSize];
+    sbtnTextHt = textHeights[sbtnTxtSize];
+    sbtnWidth = XPAD + sbtnTextWd + XPAD;
+    sbtnHeight = YPAD + sbtnTextHt + YPAD;
+}
 
 //=================================================================================================
 void ssd1306_btn::render(String nText, bool nState)
@@ -75,19 +118,19 @@ void ssd1306_btn::render(String nText, bool nState)
 
     if (sbtnEnable)
     {
+        sbtnDisplay->setTextSize(sbtnTxtSize);
         if (sbtnState)
         {
-            sbtnDisplay->fillRect(sbtnX, sbtnY, sbtnWidth, sbtnHeight, SSD1306_WHITE);
-            sbtnDisplay->setTextColor(SSD1306_INVERSE); // Draw white text
-            sbtnDisplay->setCursor(sbtnTxtX, sbtnY);       // Start at top-left corner
-            renderText();
+            sbtnDisplay->fillRect(sbtnX, sbtnY, sbtnWidth, sbtnHeight, sbtnTextColor);
+            sbtnDisplay->setTextColor(sbtnBGColor); // Draw white text
         }
         else
         {
-            sbtnDisplay->drawRect(sbtnX, sbtnY, sbtnWidth, sbtnHeight, SSD1306_WHITE);
-            sbtnDisplay->setTextColor(SSD1306_WHITE); // Draw white text
-            sbtnDisplay->setCursor(sbtnTxtX, sbtnY);     // Start at top-left corner
-            renderText();
+            sbtnDisplay->fillRect(sbtnX, sbtnY, sbtnWidth, sbtnHeight, sbtnBGColor);
+            sbtnDisplay->setTextColor(sbtnTextColor);  // Draw white text
         }
+        sbtnDisplay->drawRect(sbtnX, sbtnY, sbtnWidth, sbtnHeight, sbtnBGColor);
+        sbtnDisplay->setCursor(sbtnTxtX, sbtnTxtY);  // Start at top-left corner
+        renderText();
     }
 }
--- ssd1306_button.h
+++ ssd1306_button.h
@@ -6,25 +6,42 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define xPAD 2
-#define YPAD 0
+#define XPAD 2
+#define YPAD 1
 
+
+enum textSizes {
+    SZ_0,
+    SZ_1,
+    SZ_2,
+    SZ_3,
+    SZ_4,
+    NUM_TEXT_SIZES
+};
 
 class ssd1306_btn {
+
     public:
         bool sbtnState;
         bool sbtnEnable;
-        int sbtnWidth;
-        int sbtnHeight;
-        int sbtnX;
-        int sbtnY;
-        int sbtnTxtX;
-        int sbtnTxtY;
+        int16_t sbtnWidth;
+        int16_t sbtnHeight;
+        int16_t sbtnX;
+        int16_t sbtnY;
+        int16_t sbtnTxtX;
+        int16_t sbtnTxtY;
+        uint16_t sbtnTxtLen;
+        int16_t sbtnTextWd;
+        int16_t sbtnTextHt;
+        uint8_t sbtnTxtSize;
         String sbtnText;
+        uint16_t sbtnTextColor;
+        uint16_t sbtnBGColor;
         Adafruit_SSD1306 *sbtnDisplay;
 
         ssd1306_btn();
-        ssd1306_btn(Adafruit_SSD1306 *nDsp, int nX, int nY, int nH, int nW, String nText, bool nState);
+        ssd1306_btn(Adafruit_SSD1306 *nDsp, int16_t nX, int16_t nY, int16_t nH, int16_t nW, String nText, bool nState);
+        ssd1306_btn(Adafruit_SSD1306 *nDsp, int16_t nX, int16_t nY, uint8_t nTxtSz, String nText, bool nState);
 
         void enable();
         void disable();
@@ -37,6 +54,7 @@ class ssd1306_btn {
 
     private:
         void constructorCore();
+        void calcTextDims();
 };
 
 #endif
